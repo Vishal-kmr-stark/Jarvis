418cc8402b8a69cc4cb3279b8469bd29b4b21d20
Vishal-kmr-stark <75317254+Vishal-kmr-stark@users.noreply.github.com>
2020-12-18 21:59:03 +0530

    Update my_jarvis.py

--- my_jarvis.py
+++ my_jarvis.py
@@ -49,7 +49,7 @@ def sendEmail(to,content):
     server= smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
-    server.login("situkishaadi@gmail.com","sandeepshikha")
+    server.login("situkishaadi@gmail.com","Password")
     server.sendmail("situkishaadi@gmail.com",to,content)
     server.close()
 
